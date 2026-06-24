893e1562bde20ef016afc3b0dee514cac317b9d4
sanika931 <sanikahajare5437@gmail.com>
2026-06-24 13:16:32 +0530

    week 1 day 4

---
+++ task1.py
@@ -0,0 +1,21 @@
+name="sanika mahadev hajare"
+age=21
+std="bca 3 year"
+email="sanikahajare5437@gmail.com"
+address="at post majgaon ,tal.panalha,dis.kolhapur"
+mob_no="9209027745"
+hobby="redding and playing the chess"
+qualification="passed second year with destision"
+print(name)
+print(age)
+print(std)
+print(email)
+print(address)
+print(mob_no)
+print(hobby)
+print(qualification)
+
+
+
+
+ 
---
+++ task2.py
@@ -0,0 +1,21 @@
+name="sanika mahadev hajare"
+age=21
+std="bca 3 year"
+email="sanikahajare5437@gmail.com"
+address="at post majgaon ,tal.panalha,dis.kolhapur"
+mob_no="9209027745"
+hobby="redding and playing the chess"
+qualification="passed second year with destision"
+print(type (name),name)
+print(type (age),age)
+print(type (std),std)
+print(type (email),email)
+print(type (address),address)
+print(type (mob_no),mob_no)
+print(type (hobby),hobby)
+print(type (qualification),qualification)
+
+
+
+
+ 
